524f158d08c7f4e66b040caa21409ef1d5175764
keyu <keyurdevani2003@gmail.com>
2025-12-23 10:30:00 +0530

    all

---
+++ CLASSWORK/function datatype.py
@@ -0,0 +1,93 @@
+# a=int(input("enter a number :"))
+# print(a)
+# name= "keyu"
+# print(type(name))
+
+# l=[10,20,30,40,50,60,"s"]
+# print(l)
+# print(type(l))
+
+# #tupple
+# t=(10,20,30,40,"k")
+# print(t)
+# print(type(t))
+      
+# st ={"name":"tops","email"}
+# print(st)
+# print(type(st))
+
+# s={10,20,30,40,50,60}
+# print(s)
+# print(type(s))
+
+# #oprators
+# #arethmatic oprator
+# a = 10
+# b=20
+# print(a+b)
+
+# print("ab"+"cd")
+
+# print("abc"+str(a))
+
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(10//3)
+# print(10**3)
+
+# #assingment oprator 
+
+# a=10
+# a+=10
+# a-=10
+# a*=20
+# a/=2
+# a**=2
+# a//=2
+
+
+# #comparosion oprator
+# a=10
+# b=20
+# c=10
+
+# print(a==b)
+# print(a==c)
+# print(a>b)
+# print(a>=c)
+
+# #logical oprator
+# #and, or, not
+
+# print(true and false)
+# print(true and false)
+# print(false and true)
+# print(false and false)
+
+# print(true or Fal)
+# print(false or true)
+# print(false or false)
+# print(false or Fal)
+
+# identity oprator
+#is not is
+
+# a=10
+# b=10
+
+# print(a==b)
+# print(a is not b)
+# print(a is b)
+
+#membership optrator
+# a=[10,20,30]
+# print(10 in a)
+
+
+
+
+
+
+
+
