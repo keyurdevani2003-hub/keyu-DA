e082aa49b2987efbede10b17f1eb11f19e0cad8c
keyu <keyurdevani2003@gmail.com>
2025-12-26 13:10:04 +0530

    all

---
+++ CLASSWORK/if else.py
@@ -0,0 +1,43 @@
+# a= int(input("Enter a marks :"))
+# if a>=91 and a<=100:
+#     print("Psaa with a grade")
+# elif a>=71 and a<=90:
+#     print("pass with b grade")
+# elif a>=51 and a<=70:
+#     print("pass with c grade")
+# elif a>=35 and a<=50:
+#     print("pass with d grade")
+# elif a<34:
+#     print("fail")
+
+
+# choise=int(input("enter choice :"))
+# match choise:
+#     case 1 :
+#         print("gujrati")
+#     case 2 :
+#         print("hindi")
+#     case 3 :
+#         print("english")
+#     case 4 :
+#         print("invalid choise")
+
+
+# a=int(input("enter a number :"))
+# b=int(input("enter a number :"))
+# choise=int(input("enter a choise :"))
+# match choise:
+#     case 1 :
+#         print(a+b)
+#     case 2 :
+#         print(a-b)
+#     case 3 :
+#         print(a*b)
+#     case 4 :
+#         print(a/b)
+
+
+
+        
+
+
