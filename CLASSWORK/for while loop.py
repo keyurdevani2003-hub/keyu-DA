2c65ccc9dd676e2cbc72f8945bdd986d280d2923
keyu <keyurdevani2003@gmail.com>
2025-12-27 10:50:28 +0530

    all

---
+++ CLASSWORK/for while loop.py
@@ -0,0 +1,18 @@
+a=int(input("Enter a number :"))
+for i in range (1,11):
+    i=i*a
+    print(i)
+
+#break,continue, pass
+
+for i in range(10):
+    if i==5:
+        #break
+        #continue
+        pass
+    print(i)
+
+  
+
+
+
