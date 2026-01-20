9c1f245dcb156d958d3ac30f4b246d17c6789a24
keyu <keyurdevani2003@gmail.com>
2026-01-20 10:49:08 +0530

    all

--- CLASSWORK/Function.py
+++ CLASSWORK/Function.py
@@ -1,1 +1,94 @@
+# def get_msg():
+#     print("Hello")
+
+# def sum(a,b):
+#     print(f"Sum of a and b is {a+b}")
+    
+
+# def square(a):
+#     sq= a*a
+#     return  sq
+
+# def total(a,b,c):
+#     return a+b+c
+
+# def per(a):
+#     print(a*100/150)
+
+# t = total(30,15,60)
+
+
+# per(t)
+
+
+# def person(name,email,phone):
+#     print(name,email,phone)
+
+# # difualt argument
+
+
+# def person(name,email="dd",phone="00"):
+#     print(name,email,phone)     
+
+
+#  key word argument
+
+
+# def person(name,email="dd",phone="00"):
+#     print(name,email,phone) 
+
+# person("k",phone="1564656")
+
+
+
+
+#Function with arbitary argument with tupple
+
+
+# def sum(*a):
+#     sum=0
+#     for i in a:
+#         sum += i
+#     print(sum)
+
+# sum(10,20,30) 
+
+
+
+#  Function with arbitary argument with dictionary
+
+# def stu(**a):
+#     print(a)
+
+
+# stu(name="sagar",email="sagar@123")
+
+
+
+# lamda
+# square=lambda a:a*a
+# print(square(10))
+
+
+
+
+
+# scope  
+
+# a= 10
+# def test():
+#     global a
+#     a=20
+#     print(f"inside: {a}")
+
+# print(a)
+# test()
+# print(a)
+
+
+
+
+
+
+
 
--- CLASSWORK/for while loop.py
+++ CLASSWORK/for while loop.py
@@ -16,3 +16,4 @@ for i in range(10):
 
 
 
+
