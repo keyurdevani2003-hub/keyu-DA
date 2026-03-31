a0280c062a4a118423bc646a9f0765e80e5b8ff4
keyu <keyurdevani2003@gmail.com>
2026-03-31 12:18:21 +0530

    all

--- CLASSWORK/pandas/missing data handling.py
+++ CLASSWORK/pandas/missing data handling.py
@@ -1,7 +1,33 @@
 
 
 import pandas as pd
+import numpy as np
 
 #missing data handling
 
+# df = pd.DataFrame({"name":["aifread","batman","catwoman"],
+#                   "toy": [pd.NaT,"batmobile","bullwhip"],
+#                   "born":[pd.NaT,pd.Timestamp("1940-04-25"),None]})
+
+# print(df)
+# df1 = df.dropna()
+# print(df1)
+
+
+
+# fillna
+
+df2 = pd.DataFrame([[np.nan,2,np.nan,0],
+                   [3,4,np.nan,1],
+                   [np.nan,np.nan,np.nan,np.nan],
+                    [np.nan,3,np.nan,4]],
+                   columns=["A","B","C","D"])
+
+# print(df2)
+# df3=df2.fillna(1)
+# print(df3)
+
+df2["A"]= df2["A"].fillna(df2["A"].mean())
+df2["C"]= df2["C"].fillna("-")
+print(df2)
 
---
+++ CLASSWORK/pandas/multi indexing.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+
+# multiindexing series
+
+
+# index1 = [("california",2010),("california",2011),("new yourk",2010),("new york",2011),("texas",2010),("texas",2011)]
+# population = [154644,65464564,4654656,1654654,468615,4654656]
+
+# x = pd.Series(population,name="population",index = index1)
+# print(x)
+
+
+
+# index2 = [("a",1),("a",2),("a",3),("b",1),("b",2),("c",1)]
+# marks = [45,60,8,0,45,45]
+
+# x2 = pd.Series(marks,name="marks",index=index2)
+
+# print(x2)
+
+
+
+
+# multi indexing data frame
+
+
+
+# abc = [["A","A","A","B","B","B"],[1,2,3,1,2,3]]
+abc = [("a",1),("a",2),("a",3),("b",1),("b",2),("c",1)]
+# abc = [["A","B","C"],[1,2,3]]
+
+index3 = pd.MultiIndex.from_tuples(abc,names=("class","roll no"))
+df = pd.DataFrame({"marks" : [10,20,30,40,50,60]},index=index3)
+print(df)
+
+
+
+
+
+
+# drop level for index delete  
+
+# df = df.droplevel("roll no")
+# print(df)
+
+
+
+
+
+# reset index old index becomes columns give standard index
+df1 = df.reset_index()
+print(df1)
+
+
+
+
+
+
+
+# transpose columns and raw name change
+df2 = df1.transpose()
+print(df2)
+
+df3 = df1.T
+print(df3)
+
+
