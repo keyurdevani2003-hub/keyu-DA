806675d29aa8d6da5c3f5a4489206514b88eab48
keyu <keyurdevani2003@gmail.com>
2026-04-09 21:47:36 +0530

    all

---
+++ CLASSWORK/matplotlib seaborn/matplotlib seaborn graph.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# category wise comparision  dikana ho tab bar chart ya column chart
+# date or time wise trend batana ho to line chart area chart
+#single column ka bifercation show karna ho to pi chart donate chart
+# more than one colunms ka bofercation ke liye sun bust chart  ya tree map chart
+#geographical data hai tab  use map chart me dikha sakte hai
+# do numerical column ka relation dikhana ho scatter plot 
+# outlier ditact ke liye box plot
+#frequency dikha ne ke liye histogram ya kde plot
+# funnel chart
+
+
+
+
+
+
+
+
+df = pd.read_csv("C:/Users/HP/OneDrive/Desktop/diamonds.csv")
+print(df)
+
+
+# x = df.groupby("cut")["price"].sum()
+# print(x)
+# y1= x.reset_index()
+# print(y1)
+
+
+# plt.figure(figsize=(10,5))
+# plt.plot(y["cut"],y["price"],markers ="0",color="red",linestyle= "--")
+# plt.title("cut wise sales")
+# plt.xlabel("cut")
+# plt.ylabel("price")
+# plt.grid(True)
+# plt.show()
+
+
+
+
+# sns.lineplot(data = y1,x="cut",y="price",markers="0",color="green")
+# plt.title("cut wise price")
+# plt.grid(True)
+# plt.show()
+
+
+
+# scatter chart
+# x1 = df.groupby('carat')["price"].mean()
+# x2 = x1.reset_index()
+# print(x2)
+
+
+# plt.scatter(x2['carat'],x2["price"],alpha=0.3)
+# plt.show()
+# sns.scatterplot(data=x2,x="carat",y="price",alpha=0.5,color="purple")
+# plt.show()
+
+
+
+# bar chart
+# plt.bar(y1["cut"],y1["price"])
+# plt.show()
+
+# sns.barplot(data=y1,x="cut",y="price")
+# plt.show()
+
+
+
+# cluster bar
+# k = df.groupby(["clarity","cut"])["price"].sum()
+# k1 = k.reset_index()
+# print(k1)
+# sns.barplot(data=k1,x="cut",y="price",hue="clarity")
+# plt.show()
+
+
+# histogram chart  count the columns value or frequency
+# plt.hist(df["price"],bins=50,color="skyblue")
+# plt.show()
+
+# sns.histplot(data=df["price"])
+# plt.show()
+
+
+
+# gives probablity 
+# sns.kdeplot(data=df["price"])
+# sns.histplot(data=df["price"],kde=True)
+# plt.show()
+
+
+#  boxplot for finding outlier 
+# sns.boxplot(data=df["price"])
+# sns.boxplot(data=df,x="cut",y="price",hue="cut")
+# plt.show()
+
+
+
+# violine plot mix chart of boxplot  kdfplot
+# sns.violinplot(x="clarity",y="price",data=df)
+# plt.show()
+
+
+
+# stack bar chart
+
+# using seaborn
+import seaborn.objects as so
+# so.Plot(k1,x="clarity",y="price",color="cut").add(so.Bar()).scale(color="Set2").show()
+
+
+# using matplotlib
+# x = ["A","B","C","D"]
+# y1 = [10,20,30,45]
+# y2 = [25,45,12,28]
+# y3 = [12,35,14,18]
+
+# y4 = np.add(y1,y2).tolist()
+# print(y4)
+
+# plt.bar(x,y1,color="red")
+# plt.bar(x,y2,bottom=y1,color="blue")
+# plt.bar(x,y3,bottom=y4,color="green")  
+
+# plt.show()
+
+
+
+
+
+# pi chart
+
+
+
+clarity_c = df["cut"].value_counts()
+print(clarity_c)
+
+import matplotlib.pyplot as plt
+
+plt.pie(clarity_c.values,
+        labels=clarity_c.index,
+        autopct="%1.1f%%")
+
+plt.title("Cut Distribution")
+plt.show()
---
+++ Task/02 task.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_csv("C:/Users/HP/OneDrive/Desktop/weather_classification_data.csv",encoding='latin-1')
+print(df)
+
+
+# konse weather type ke kitne record hai
+# plt.hist(df["Weather Type"],bins=10,color="skyblue")
+# plt.show()
+
+
+
+#location wise highest temprature comparision
+
+
+# x = df.groupby("Location")["Temperature"].max().reset_index()
+# print(x)
+
+# plt.bar(x["Location"], x["Temperature"]) 
+# plt.show()
+
+
+
+
+#temprature and wind spped ki frequency
+
+#plt.hist(df["Temperature"],color = "blue")
+# plt.hist(df["Wind Speed"],color="red")
+# plt.show()
+
+
+
+
+#season wise locaton wise avg atmosphere  kya hai
+
+# x = df.groupby(["Season","Location"])["Atmospheric Pressure"].mean().reset_index()
+
+# print(x)
+
+# sns.barplot(data=x,x = "Location",y="Atmospheric Pressure",hue="Season")
+# plt.show()
+
+
+
+#humidity ki density janni hai,humidity mai outliyers hai ya nahi janna hai
+
+# sns.kdeplot(data=df["Humidity"])
+# sns.boxplot(data=df["Humidity"])
+# plt.show()
+
+
+
+
+#uv index and temprature ka relation janana hai
+
+
+x  =  df.groupby("UV Index")["Temperature"].mean().reset_index()
+plt.plot(x['UV Index'],x["Temperature"])
+plt.show()
+
