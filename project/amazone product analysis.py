c82d97ce686ec0505763c4bc05b9a5c58f60554a
keyu <keyurdevani2003@gmail.com>
2026-07-15 14:59:13 +0530

    all

---
+++ project/amazone product analysis.py
@@ -0,0 +1,26 @@
+
+
+from datetime import datetime
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+df = pd.read_excel("C:/Users/HP/Downloads/Amazon_Product_Analysis_Dataset.xlsx")
+print(df)
+
+
+# x=df.isnull().sum()
+# print(x)
+
+# print(df.describe())
+
+# x = df.duplicated().sum()
+# print(x)
+
+
+# print(df.info())
+
+
